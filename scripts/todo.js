170b77964dcc1342d056094d4293e57697f53c85
MRH <mariarhennig@gmail.com>
2018-08-02 23:21:23 +0100

    work on to do list
    - working on saving, deleting & loading
    - work in progress

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,24 +2,22 @@ var todos = [];
 var close = document.getElementsByClassName("close");
 var i;
 
-window.onload=function(){
-  // Create a "close" button and append it to each list item
-  var myNodelist = document.getElementsByTagName("LI");
-  var i;
-  for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-  }
-}
+
 
 function read_todos(){
+  document.getElementById("myInput").value = "";
+  var links = document.getElementsByTagName("li");
+  console.log(links);
+  for (i in _.range(links.length)){
+    links[i].onload = function () {
+      links.style.display = "none";
+    }
+  }
+
   var todos_stored = localStorage.getItem("TodosLocalStorage");
-  console.log(todos_stored);
+  //console.log(todos_stored);
   var todos_list = JSON.parse(todos_stored);
-  //console.log(todos_list.length);
+  //console.log(todos_list);
   //console.log(todos_list[0]);
   if (todos_list === null){
     todos_list=[];
@@ -38,9 +36,13 @@ function read_todos(){
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function(){
         var div = this.parentElement;
-        var divtxt = div.innerText;
-        divtxt = divtxt.substring(0, divtxt.length - 1);
+        var divtxt_raw = div.innerText;
+        divtxt_raw = divtxt_raw.substring(0, divtxt_raw.length - 2);
         div.style.display = "none";
+        console.log(divtxt_raw);
+        var divtxt_1 = JSON.stringify(divtxt_raw);
+        var divtxt = JSON.parse(divtxt_1);
+        console.log(divtxt_1);
         console.log(divtxt);
         delete_from_list(divtxt);
       }
@@ -50,50 +52,33 @@ function read_todos(){
 }
 
 function newElement() {
-  var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
     // read & add existing items to array
     var todos_stored = localStorage.getItem("TodosLocalStorage");
     var todos_list = JSON.parse(todos_stored);
-    console.log(todos_stored);
+    //console.log(todos_stored);
     if (todos_list === null){
         todos = [];
     } else {
       todos = [];
-      for (i=0; i<todos_list.length; i++){
+      for (i in _.range(todos_list.length)){
         todos.push(todos_list[i]);
+        console.log(todos);
       }
     }
     // Add new input to array
     todos.push(inputValue);
-    //console.log(todos);
+    console.log(todos);
     var new_todos = JSON.stringify(todos);
     //console.log(new_todos);
     localStorage.setItem("TodosLocalStorage", new_todos);
     //var todos_stored = localStorage.getItem("TodosLocalStorage");
-    console.log(todos_stored);
+    //console.log(todos_stored);
   }
-  document.getElementById("myInput").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  
-  
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-
+  read_todos();
 }
 
 function delete_from_list(divtxt){
@@ -104,15 +89,14 @@ function delete_from_list(divtxt){
 
   // find the index of the clicked item and remove it
   var index = todos_list.indexOf(divtxt);
+  //console.log(todos_list);
   console.log(index);
-  if (index > -2) {
+  if (index != null) {
     todos_list.splice(index,1);
-    console.log(todos_list)
-  }
+    console.log(todos_list);
+  } 
   // save the new list in local storage
   var new_todos = JSON.stringify(todos_list);
   console.log(new_todos);
   localStorage.setItem("TodosLocalStorage", new_todos);
-  //make invisible on page
-  
 }
